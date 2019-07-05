a6456d86c0646360326112ca5c8bab13e6aefaa0
dvd42 <diegovd0296@gmail.com>
2019-07-05 17:15:06 +0200

    oid dataloader

---
+++ maskrcnn_benchmark/data/datasets/evaluation/oid/__init__.py
@@ -0,0 +1,21 @@
+from .oid_eval import do_oid_evaluation
+
+
+def oid_evaluation(
+    dataset,
+    predictions,
+    output_folder,
+    box_only,
+    iou_types,
+    expected_results,
+    expected_results_sigma_tol,
+):
+    return do_oid_evaluation(
+        dataset=dataset,
+        predictions=predictions,
+        box_only=box_only,
+        output_folder=output_folder,
+        iou_types=iou_types,
+        expected_results=expected_results,
+        expected_results_sigma_tol=expected_results_sigma_tol,
+    )
